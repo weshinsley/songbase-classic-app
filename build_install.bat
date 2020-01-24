2f581fd449537abbdc69614c3ff588dcc915d806
Wes Hinsley <wes.hinsley@gmail.com>
2020-01-24 09:53:27 +0000

    Fix some install build bugs

--- build_install.bat
+++ build_install.bat
@@ -5,16 +5,16 @@ copy ..\Songbase-Manual.pdf
 copy ..\*.ocx
 del Songbase.exe
 copy ..\Songbase.exe Songbase.exe
-..\upx Songbase.exe
+..\tools\upx Songbase.exe
 copy ..\Songbase.exe.manifest
-..\mt.exe -manifest "Songbase.exe.manifest" -outputresource:"Songbase.exe";#1
+..\tools\mt.exe -manifest "Songbase.exe.manifest" -outputresource:"Songbase.exe";#1
 del Songbase.exe.manifest
 if not exist html mkdir html
-copy ..\songbase.exe /y
+copy ..\Songbase.exe /y
 copy ..\CHANGES.TXT /y
 copy ..\en_GB.xml /y
 copy ..\TODO.TXT /y
 copy ..\html\*.html html /y
-copy ..\..\songbase-classic-viewer\Viewer.exe .. /y
-copy ..\..\songbase-classic-viewer\Viewer.ini .. /y
+copy ..\..\songbase-classic-viewer\Viewer.exe /y
+copy ..\..\songbase-classic-viewer\Viewer.ini /y
 cd ..
