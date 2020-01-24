818c36ec16957d9a922a5d91916fdf159d90adb9
Wes Hinsley <wes.hinsley@gmail.com>
2020-01-24 09:50:08 +0000

    Move things around for better install build

--- build_install.bat
+++ build_install.bat
@@ -1,4 +1,20 @@
-mt.exe -manifest "songbase.exe.manifest" -outputresource:"songbase.exe";#1
-upx songbase.exe
+if not exist install mkdir install
 cd install
+copy ..\*.dll
+copy ..\Songbase-Manual.pdf
+copy ..\*.ocx
+del Songbase.exe
+copy ..\Songbase.exe Songbase.exe
+..\upx Songbase.exe
+copy ..\Songbase.exe.manifest
+..\mt.exe -manifest "Songbase.exe.manifest" -outputresource:"Songbase.exe";#1
+del Songbase.exe.manifest
+if not exist html mkdir html
 copy ..\songbase.exe /y
+copy ..\CHANGES.TXT /y
+copy ..\en_GB.xml /y
+copy ..\TODO.TXT /y
+copy ..\html\*.html html /y
+copy ..\..\songbase-classic-viewer\Viewer.exe .. /y
+copy ..\..\songbase-classic-viewer\Viewer.ini .. /y
+cd ..
