a1e4776089f12b9e46cccb3054044d5c775ba616
Wes Hinsley <wes.hinsley@gmail.com>
2020-01-24 09:59:10 +0000

    Working!

--- build_install.bat
+++ build_install.bat
@@ -1,19 +1,19 @@
 if not exist install mkdir install
 cd install
-copy ..\*.dll
-copy ..\Songbase-Manual.pdf
-copy ..\*.ocx
 del Songbase.exe
 copy ..\Songbase.exe Songbase.exe
 ..\tools\upx Songbase.exe
 copy ..\Songbase.exe.manifest
 ..\tools\mt.exe -manifest "Songbase.exe.manifest" -outputresource:"Songbase.exe";#1
 del Songbase.exe.manifest
-if not exist html mkdir html
-copy ..\Songbase.exe /y
+copy ..\Songbase.ini /y
+copy ..\*.dll /y
+copy ..\*.ocx /y
+copy ..\Songbase-Manual.pdf /y
 copy ..\CHANGES.TXT /y
 copy ..\en_GB.xml /y
 copy ..\TODO.TXT /y
+if not exist html mkdir html
 copy ..\html\*.html html /y
 copy ..\..\songbase-classic-viewer\Viewer.exe /y
 copy ..\..\songbase-classic-viewer\Viewer.ini /y
